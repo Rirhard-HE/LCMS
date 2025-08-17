7a6ce0cb0723f7f60f13a7aa0b4c16d52ad5b485
Rirhard-HE <1109912480hyt@gmail.com>
2025-08-17 22:09:44 +1000

    fix workflow

--- backend/.mocharc.cjs
+++ backend/.mocharc.cjs
@@ -1,9 +1,7 @@
 // backend/.mocharc.cjs
 module.exports = {
-  require: ['./test/setup.js'],
-  spec: ['test/**/*.test.js', 'test/**/*.spec.js'],
+  require: ['./test/setup.cjs'],   // 预加载你的测试初始化
   recursive: true,
   timeout: 30000,
   exit: true,
-  color: true
 };
